9dd30dae8801fb63cb12c5020f21d40be4684837
nekram <nekram.stcs@gmail.com>
2026-07-15 14:13:43 +0530

    rg added

--- rg.tf
+++ rg.tf
@@ -1,13 +1,21 @@
+terraform {
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.80.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
 resource "azurerm_resource_group" "rg1" {
 name = "rg1"
 location = "eastus"
 }
-resource "azurerm_resource_group" "rg2" {
-name = "rg2"
-location = "Central India"
-}
-resource "azurerm_resource_group" "rg3" {
-name = "rg3"
-location = "uaecentral"
-}
+
+
 
